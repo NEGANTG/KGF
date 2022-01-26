ff6770e74a1a548bcbff3c2c9f163e10bd575a9b
linto binoy <93820158+lintobinoy007@users.noreply.github.com>
2022-01-26 19:31:00 +0530

    Update shit.py

--- Shadow/modules/shit.py
+++ Shadow/modules/shit.py
@@ -6,12 +6,12 @@ from .utils.disable import disableable_dec
 @register(cmds="jo")
 @disableable_dec("jo")
 async def _(message):
-    j = "Hello there"
+    j = "ennada narii"
     await message.reply(j)
 
 
 __mod_name__ = "jo"
 __help__ = """
 <b>jo</b>
-- /jo: fuck of dude
+- /jo: fuck
 """
