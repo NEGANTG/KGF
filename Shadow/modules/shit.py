fd6e7ce1a352e4159c468db2d824c14df897749a
Deshadeeth-Thisarana <deshadeeththisarana@gmail.com>
2022-01-26 13:55:46 +0000

    Auto Fixes: Code formatted

--- Shadow/modules/shit.py
+++ Shadow/modules/shit.py
@@ -1,13 +1,15 @@
 ﻿from Shadow.decorator import register
+
 from .utils.disable import disableable_dec
-from .utils.message import get_args_str
+
 
 @register(cmds="jo")
 @disableable_dec("jo")
 async def _(message):
     j = "Hello there"
     await message.reply(j)
-    
+
+
 __mod_name__ = "jo"
 __help__ = """
 <b>jo</b>
